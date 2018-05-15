43005a8f127482e1b8c8a399fe9bc2ac2f4b35d2
Waqas Altaf <waqasaltaf9@gmail.com>
2018-05-15 15:58:18 +0500

    c

---
+++ database/migrations/2018_05_15_105545_create_registers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->increments('id');
+              $table->String('first_name');
+              $table->String('last_name');
+              $table->String('password');
+              $table->integer('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registers');
+    }
+}
